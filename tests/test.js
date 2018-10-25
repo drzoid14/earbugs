28c72cfbbb357671d683ac9a4b215154478a5738
Billy Angermeier <william.r.angermeier@gmail.com>
2018-10-25 18:41:57 -0400

    started tests

--- tests/test.js
+++ tests/test.js
@@ -33,6 +33,48 @@ describe('earbugs', function () {
             expect(res).to.have.status(200);
         })
 
+    });
+
+    it('should return videos on GET', function() {
+        return chai.request(app)
+        .get('/videos')
+        .then(function(res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.a('array')
+        })
     })
+
+    it('should post a video on POST', function(){
+        const testVid={
+            video1: 'a',
+            video2:'b',
+            start1: '1:00',
+            start2: '0:00',
+            end1: '1:01',
+            end2: '0:01',
+            title: 'test'
+        }
+        .post('/videos')
+        .send(testVid)
+        .then(function(res){
+            expect(res).to.have.status(201);
+            expect(res).to.be.json;
+            expect(res.body.id).to.not.equal(null)
+        })
+    })
+
+    it('should delete a video on delete', function(){
+        return chai.request(app)
+
+        .get('/videos')
+        .then(function(res){
+            return chai.request(app)
+            .delete(`/videos/{${res.body[0].id}`)
+        })
+        .then(function(res){
+            expect(res).to.have.status(204)
+        });
+    });
 })
 })
